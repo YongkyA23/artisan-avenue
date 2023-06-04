e5c4365206c2ba3a9dea608e0660abe469ad2c61
YongkyA23 <67529856+YongkyA23@users.noreply.github.com>
2023-06-04 19:34:42 +0700

    js dikit

--- js/script.js
+++ js/script.js
@@ -72,3 +72,149 @@ $("#right-arrow").click(function () {
     }
   );
 });
+
+// Store the user credentials in local storage
+var accounts = [
+  { email: "example1@example.com", username: "user1", password: "password1" },
+  { email: "example2@example.com", username: "user2", password: "password2" },
+  // Add more account objects as needed
+];
+
+var accounts = JSON.parse(localStorage.getItem("accounts")) || [];
+
+var isLoggedInStatus = false;
+// Check if the user is logged in
+if (isLoggedIn()) {
+  var username = getUsername();
+  updateLoginLinkText("Hi, " + username);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var loginForm = document.getElementById("loginForm");
+  loginForm.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form submission
+
+    var usernameOrEmail = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    if (validateLogin(usernameOrEmail, password)) {
+      var username = getUsername(usernameOrEmail);
+      updateLoginLinkText("Hi, " + username);
+      isLoggedInStatus = true; // Set isLoggedInStatus to true to indicate the user is logged in
+
+      window.location.href = "index.html";
+    } else {
+      alert("Invalid username or password. Please try again.");
+    }
+  });
+});
+
+function isLoggedIn() {
+  // Check if the login status exists in session storage
+  var loginStatus = sessionStorage.getItem("isLoggedIn");
+  return loginStatus === "true"; // Return true if the user is logged in, false otherwise
+}
+
+function updateLoginLinkText(text) {
+  var loginLink = document.getElementById("loginLink");
+  if (loginLink) {
+    loginLink.textContent = text;
+  }
+}
+
+function validateLogin(usernameOrEmail, password) {
+  var accounts = JSON.parse(localStorage.getItem("accounts")) || [];
+
+  // Check if the entered username/email and password match any of the stored accounts
+  for (var i = 0; i < accounts.length; i++) {
+    var account = accounts[i];
+    if (
+      (usernameOrEmail === account.email ||
+        usernameOrEmail === account.username) &&
+      password === account.password
+    ) {
+      return true; // Login successful
+    }
+  }
+
+  return false; // Login failed
+}
+
+function getUsername(usernameOrEmail) {
+  var accounts = JSON.parse(localStorage.getItem("accounts")) || [];
+
+  // Find the account with the matching username/email
+  var account = accounts.find(function (acc) {
+    return usernameOrEmail === acc.email || usernameOrEmail === acc.username;
+  });
+
+  // Return the username if found, otherwise return an empty string
+  return account ? account.username : "";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var registerForm = document.getElementById("registerForm");
+  registerForm.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form submission
+
+    var firstName = document.getElementById("firstname").value;
+    var lastName = document.getElementById("lastname").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    console.log(firstName, lastName, email, password);
+
+    if (validateRegistration(firstName, lastName, email, password)) {
+      var username = generateUsername(firstName, lastName);
+      var account = { username: username, email: email, password: password };
+      registerAccount(account);
+      alert(
+        "Registration successful. Your username is: " +
+          username +
+          ". Please sign in to continue."
+      );
+      window.location.href = "signin.html";
+      console.log("After:", accounts); // Redirect to the login page after successful registration
+    } else {
+      alert("Invalid registration details. Please try again.");
+    }
+  });
+});
+
+function validateRegistration(firstName, lastName, email, password) {
+  if (
+    firstName.trim() === "" ||
+    lastName.trim() === "" ||
+    email.trim() === "" ||
+    password.trim() === ""
+  ) {
+    return false;
+  }
+
+  return true;
+}
+
+function generateUsername(firstName, lastName) {
+  // Generate a username based on the first name and last name
+  // You can customize the logic to generate the username as per your requirements
+  // For example, concatenate the first name and last name, or use a combination of letters and numbers
+
+  // Convert the first name and last name to lowercase and remove any spaces
+  var username = (firstName.toLowerCase() + lastName.toLowerCase()).replace(
+    /\s/g,
+    ""
+  );
+
+  return username; // Return the generated username
+}
+
+function registerAccount(account) {
+  var accounts = JSON.parse(localStorage.getItem("accounts")) || [];
+
+  console.log("Before:", accounts); // Check the existing accounts
+
+  accounts.push(account);
+  localStorage.setItem("accounts", JSON.stringify(accounts));
+
+  console.log("After:", accounts); // Check if the new account is added
+}
